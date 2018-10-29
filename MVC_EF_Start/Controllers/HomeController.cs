953d97f262b15c859190d16986b24fb1eacc20cd
Dominique Rocco <domroc94@yahoo.com>
2018-10-29 16:59:04 -0600

    class

--- MVC_EF_Start/Controllers/HomeController.cs
+++ MVC_EF_Start/Controllers/HomeController.cs
@@ -4,9 +4,25 @@ namespace MVC_EF_Start.Controllers
 {
   public class HomeController : Controller
   {
+    public ApplicationDbContext dbContext;
+
+    public HomeController(ApplicationDbContext context)
+    {
+      dbContext = context;
+    }
+
     public IActionResult Index()
     {
-      return View();
+      Company MyCompany = new Company();
+      MyCompany.symbol = "ISM6225";
+      MyCompany.name = "ISM";
+      MyCompany.date = "ISM";
+      MyCompany.isEnabled = true;
+      MyCompany.type = "ISM";
+      MyCompany.iexId = "ISM";
+
+      dbContext.Companies.Add(MyCompany);
+      dbContext.SaveChanges();
     }
   }
 }
